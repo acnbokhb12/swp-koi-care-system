60c0cf2a7d2449995155d2bbab6b730ae8954cdb
Khanhhnse183037 <huynhngockhanh1201@gmail.com>
2024-10-11 14:38:27 +0700

    logingg

--- src/java/com/swp/koiCareSystem/config/IConstant.java
+++ src/java/com/swp/koiCareSystem/config/IConstant.java
@@ -32,11 +32,15 @@ public interface IConstant {
     public final String PONDINFOR = "pondinfor"; 
     
     
+    Dotenv dotenv = Dotenv.configure().directory(".").filename(".env").load();
+     
+    public static final String GOOGLE_CLIENT_ID = dotenv.get("GOOGLE_CLIENT_ID");
+    public static final String GOOGLE_CLIENT_SECRET = dotenv.get("GOOGLE_CLIENT_SECRET");
+    public static final String GOOGLE_REDIRECT_URI = dotenv.get("GOOGLE_REDIRECT_URI");
+    public static final String GOOGLE_GRANT_TYPE = dotenv.get("GOOGLE_GRANT_TYPE");
+    public static final String GOOGLE_LINK_GET_TOKEN = dotenv.get("GOOGLE_LINK_GET_TOKEN");
+    public static final String GOOGLE_LINK_GET_USER_INFO = dotenv.get("GOOGLE_LINK_GET_USER_INFO");
     
-//    Dotenv dotenv = Dotenv.configure()
-//    .directory("D:\\f_code\\Project_Swp_Fall_2024\\com.swp.koiCareSystem\\web\\WEB-INF") // Đường dẫn tuyệt đối
-//    .filename("devcontainer.env")
-//    .load();\
     
      
 }
